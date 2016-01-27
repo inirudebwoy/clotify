59388c2a884ef28a888bf8717730f6b7f039e726
Michael Klich <michal@michalklich.com>
2016-01-27 21:55:02 +0000

    Research how to add headers to response

--- src/clotify/core.clj
+++ src/clotify/core.clj
@@ -1,20 +1,28 @@
 (ns clotify.core
   (:require [liberator.core :refer [resource defresource]]
+            [liberator.representation :refer [as-response]]
             [ring.middleware.params :refer [wrap-params]]
             [compojure.core :refer [defroutes context GET POST]]))
 
-;; routes
+
+(defn get-playlist-tracks [id] (format "Track list for playlist: %s" id))
+
+
+;;; routes
 ;; GET all tracks in current playlist
 (defresource playlist [id]
   :available-media-types ["application/json"]
-  :handle-ok (format "Track list for playlist: %s" id)
-  :authorized? (fn [_] "Not authorized"))
+  :handle-ok (get-playlist-tracks id))
 
 ;; POST new track to end of playlist
+;; TODO: response status should be based on if track could be added
 (defresource playlist-add []
   :allowed-methods [:post]
   :available-media-types ["application/json"]
-  :handle-ok "You have added track to playlist")
+  :handle-ok "You have added track to playlist"
+  :as-response (fn [d ctx]
+                 (-> (as-response d ctx)
+                     (assoc-in [:headers "X-Response-Type"] "SUCCESS"))))
 
 ;; POST skip currently running track
 (defresource playlist-skip []
